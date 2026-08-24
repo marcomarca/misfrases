1bf12f1c04d30c73a3c61c91887bd82d33509351
marcomarca <timburtonmarco@gmail.com>
2026-08-23 20:03:50 -0400

    build(forge): configure output directory to release folder

--- forge.config.ts
+++ forge.config.ts
@@ -4,6 +4,7 @@ import { MakerZIP } from '@electron-forge/maker-zip';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 
 const config: ForgeConfig = {
+  outDir: 'release',
   packagerConfig: {
     asar: true,
     name: 'MisFrases',
